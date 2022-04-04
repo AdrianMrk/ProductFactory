8d7d95433c0cf326045ca5e22562e500355d238d
AdrianMarkiewicz <amarkiewicz27@gmail.com>
2022-04-04 18:36:53 +0200

    fix

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f2dbf7da715466e7"; }
+/******/ 		__webpack_require__.h = function() { return "4d22b954f2af60f2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
